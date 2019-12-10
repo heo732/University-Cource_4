70de731f6483e762edb5eaebb95053d598b189d4
Andrii Buzhak <andriy.buzhak.1@gmail.com>
2019-12-10 23:00:33 +0200

    Lab_1 and Lab_2; not sure that it works.

---
+++ InformationTheoryAndCoding/Lab_1/TeoriyaInformatsiyiTaKoduvannyaLab1/Program.cs
@@ -0,0 +1,182 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TeoriyaInformatsiyiTaKoduvannyaLab1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.InputEncoding = Encoding.Unicode;
+            Console.OutputEncoding = Encoding.Unicode;
+
+            int N = 4;
+            double[,] P = new double[N, N];
+
+            double[] pA = new double[N];
+            double[] pB = new double[N];
+
+            double HA = 0, HB = 0;
+
+            P[0, 0] = 0.27;
+            P[0, 1] = 0.01;
+            P[0, 2] = 0.07;
+            P[0, 3] = 0.04;
+            
+            P[1, 0] = 0;
+            P[1, 1] = 0.1;
+            P[1, 2] = 0.04;
+            P[1, 3] = 0.02;
+
+            P[2, 0] = 0.02;
+            P[2, 1] = 0.03;
+            P[2, 2] = 0.1;
+            P[2, 3] = 0;
+
+            P[3, 0] = 0.07;
+            P[3, 1] = 0.02;
+            P[3, 2] = 0.11;
+            P[3, 3] = 0.1;
+
+            Console.WriteLine("Матриця сумісних ймовірностей \n");
+
+            for (int ai = 0; ai < N; ai++, Console.WriteLine())
+                for (int bj = 0; bj < N; bj++)
+                {
+                    pA[ai] += P[ai, bj];
+                    pB[bj] += P[ai, bj];
+                    Console.Write("\t" + P[ai, bj]);
+                }
+
+            Console.WriteLine("\n Розподіл ймовірностей джерела А (P[ai]):\n");
+
+            for (int ai = 0; ai < N; ai++)
+            {
+                HA += pA[ai] * Math.Log(pA[ai], 2);
+                Console.Write("\n" + pA[ai]);
+            }
+
+            HA = -HA;
+            Console.WriteLine();
+
+
+            Console.WriteLine();
+            Console.WriteLine("\n Безумовна ентропія H(A) = " + HA);
+
+
+            Console.WriteLine("\n Розподіл ймовірностей джерела В (P[ai]):\n");
+
+            for (int bj = 0; bj < N; bj++)
+            {
+                HB += pB[bj] * Math.Log(pB[bj], 2);
+                Console.Write("\n" + pB[bj]);
+            }
+
+            HB = -HB;
+
+
+            Console.WriteLine();
+            Console.WriteLine(" Безумовна ентропія H(B) = " + HB);
+
+            Console.WriteLine();
+
+            double[,] Paibj = new double[N, N];
+            double[,] Pbjai = new double[N, N];
+
+            for (int ai = 0; ai < N; ai++)
+                for (int bj = 0; bj < N; bj++)
+                {
+                    Paibj[ai, bj] = P[ai, bj] / pB[bj];
+                    Pbjai[ai, bj] = P[ai, bj] / pA[ai];
+                }
+            
+            Console.WriteLine("\n Матриця умовних ймовірностей джерела А - p(ai/bj):\n");
+
+            for (int ai = 0; ai < N; ai++, Console.WriteLine())
+                for (int bj = 0; bj < N; bj++)
+                {
+                    Console.Write(Paibj[ai, bj].ToString().Length < 5 ? "\t" + Paibj[ai, bj] : "\t" + Paibj[ai, bj].ToString().Substring(0, 5));
+                }
+
+            Console.WriteLine("\n\n Матриця умовних ймовірностей джерела В - p(bj/ai):\n");
+
+            for (int ai = 0; ai < N; ai++, Console.WriteLine())
+                for (int bj = 0; bj < N; bj++)
+                {
+                    Console.Write(Pbjai[ai, bj].ToString().Length < 5 ? "\t" + Pbjai[ai, bj] : "\t" + Pbjai[ai, bj].ToString().Substring(0, 5));
+                }
+
+            double[] HAbj = new double[N];
+            double[] HBai = new double[N];
+
+
+            for (int ai = 0; ai < N; ai++)
+            {
+                HAbj[ai] = 0;
+                HBai[ai] = 0;
+            }
+
+            for (int ai = 0; ai < N; ai++)
+                for (int bj = 0; bj < N; bj++)
+                {
+                    if (Paibj[ai, bj] > 0)
+                        HAbj[bj] += -(Paibj[ai, bj] * Math.Log(Paibj[ai, bj], 2));
+
+                    if (Pbjai[ai, bj] > 0)
+                        HBai[ai] += -(Pbjai[ai, bj] * Math.Log(Pbjai[ai, bj], 2));
+                    
+                }
+
+            Console.WriteLine("\n Часткова умовна ентропія джерела А - H(A/bj):\n");
+
+            for (int ai = 0; ai < N; ai++)
+                {
+                    Console.Write(HAbj[ai].ToString().Length < 5 ? "\t" + HAbj[ai] : "\t" + HAbj[ai].ToString().Substring(0, 5));
+                }
+
+            Console.WriteLine("\n\n Часткова умовна ентропія джерела В - H(B/ai):\n");
+
+            for (int ai = 0; ai < N; ai++)
+            {
+                Console.Write(HBai[ai].ToString().Length < 5 ? "\t" + HBai[ai] : "\t" + HBai[ai].ToString().Substring(0, 5));
+            }
+
+            double HAB = 0, HBA = 0;
+
+            for (int bj = 0; bj < N; bj++)
+            {
+                HAB += pB[bj] * HAbj[bj];
+                HBA += pA[bj] * HBai[bj];
+            }
+
+            Console.WriteLine("\n\n Загальна умовна ентропія - H(A/B) = " + HAB.ToString().Substring(0, 5));
+            Console.WriteLine("\n Загальна умовна ентропія - H(B/A) = " + HBA.ToString().Substring(0, 5));
+
+            double Hab = 0;
+            Hab = HA + HBA;
+
+            Console.WriteLine("\n\n Ентропія об'єднання - H(A,B) = " + Hab.ToString().Substring(0, 5));
+
+            double Hab1 = 0;
+            Hab1 = HB + HAB;
+
+            Console.WriteLine("\n\n Ентропія об'єднання (другий спосіб) - H(A,B) = " + Hab1.ToString().Substring(0, 5));
+
+            Console.WriteLine("\n\n Висновок: кількість інформації, що припадає на пару повідомлень ai, bj = " + Hab1.ToString().Substring(0, 5) + " біт");
+
+            double IAB = 0;
+            IAB = HA - HAB;
+
+            Console.WriteLine("\n\n Повна взаємна інформація I(A,B) = " + IAB.ToString().Substring(0, 5));
+
+            Console.WriteLine("\n\n Висновок: кількість інформації, що містить в середньому символ на виході першого джерела про виникнення символів на виході другого джерела становить " + IAB.ToString().Substring(0, 5) + " біт");
+
+            Console.WriteLine();
+            Console.ReadKey();
+        }
+    }
+}
---
+++ InformationTheoryAndCoding/Lab_2/Lab_2_Theory_of_coding/Program.cs
@@ -0,0 +1,186 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace Lab_2_Theory_of_coding
+{
+    class Program
+    {
+        
+
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = System.Text.Encoding.Unicode;
+            Console.InputEncoding = System.Text.Encoding.Unicode;
+
+            int Np = 4, n = 3; //Np - кількість робочих комбінацій, n - довжина коду
+            Console.WriteLine("Кодові комбінації:\n V1=001;\n V2=010;\n V3=011;\n V4=100; ");
+            int[] V1 = { 0, 0, 1 };
+            int[] V2 = { 0, 1, 0 };
+            int[] V3 = { 0, 1, 1 };
+            int[] V4 = { 1, 0, 0 };
+            int[][] V = new int[6][] { new int[3], new int[3], new int[3], new int[3], new int[3], new int[3] };
+            for (int i = 0; i < 3; i++)
+            {
+                V[0][i] = V1[i] ^ V2[i];
+                V[1][i] = V1[i] ^ V3[i];
+                V[2][i] = V1[i] ^ V4[i];
+                V[3][i] = V2[i] ^ V3[i];
+                V[4][i] = V2[i] ^ V4[i];
+                V[5][i] = V3[i] ^ V4[i];
+            }
+            Console.WriteLine();
+            Console.WriteLine("Додавання за модулем 2 ");
+            for (int i = 0; i < V.Length; i++)
+            {
+                Console.Write("V[" + i + "] ");
+                for (int j = 0; j < 3; j++)
+                {
+                    Console.Write(V[i][j] + "  ");
+                }
+            }
+            Console.WriteLine();
+            int[] d = new int[V.Length];
+            for (int i = 0; i < V.Length; i++)
+            {
+                int count = 0;
+                for (int j = 0; j < 3; j++)
+                {
+                    if (V[i][j] == 1)
+                        count++;
+                }
+                d[i] = count; //вектор кодових відстаней
+            }
+            Console.WriteLine();
+
+            Console.WriteLine("Кодові відстані");
+            for (int i = 0; i < d.Length; i++)
+            {
+                Console.Write("  d[" + i + "] " + d[i]);
+            }
+            Console.WriteLine();
+            
+            Console.WriteLine();
+            int[,] kd = { { 0, 2, 1, 2 }, { 2, 0, 1, 2 }, { 1, 1, 0, 3 }, { 2, 2, 3, 0 } }; //таблиця кодових відстаней
+            Console.WriteLine("Таблиця кодових відстаней");
+
+            for (int i = 0; i < Np; i++)
+            {
+                for (int j = 0; j < Np; j++)
+                {
+                    Console.Write(kd[i, j] + " ");
+                }
+                Console.WriteLine();
+            }
+
+            Console.WriteLine();
+            int[] Npi = new int[4]; //кількість робочих комбінацій, які знаходться на кодовій відстані D від i-тої робочї комбінації
+            for (int l = 0; l < Np; l++)
+            {
+                for (int i = 0; i < Np; i++)
+                {
+                    int count = 0;
+                    for (int j = 0; j < Np; j++)
+                    {
+                        if (kd[i, j] == 1)
+                            count++;
+                    }
+                    Npi[i] = count;
+                }
+                for (int i = 0; i < Np; i++)
+                {
+                    int count = 0;
+                    for (int j = 0; j < Np; j++)
+                    {
+                        if (kd[i, j] == 2)
+                            count++;
+                    }
+                    Npi[i] = count;
+                }
+                for (int i = 0; i < Np; i++)
+                {
+                    int count = 0;
+                    for (int j = 0; j < Np; j++)
+                    {
+                        if (kd[i, j] == 3)
+                            count++;
+                    }
+                    Npi[i] = count; ;
+                }
+                for (int i = 0; i < Np; i++)
+                {
+                    int count = 0;
+                    for (int j = 0; j < Np; j++)
+                    {
+                        if (kd[i, j] == 4)
+                            count++;
+                    }
+                    Npi[i] = count;
+                }
+            }
+            int[] NPi = { 4, 4, 6, 2, 0 };
+            Console.WriteLine("Кількість робочих комбінацій");
+            for (int i = 0; i < NPi.Length; i++)
+            {
+                Console.Write("   Npi[" + i + "] = " + NPi[i] +"\n");
+            }
+            Console.WriteLine();
+            int temp = 0;
+            
+            for (int i = 0; i < NPi.Length; i++)
+            {
+                for (int j = 0; j < NPi.Length - 1; j++)
+                {
+
+                    if (NPi[j] > NPi[j + 1])
+                    {
+                        temp = NPi[j];
+                        NPi[j] = NPi[j + 1];
+                        NPi[j + 1] = temp;
+                    }
+                }
+            }
+            Console.WriteLine();
+
+            int[] NPi1 = { 4, 4, 6, 2, 0 };
+
+
+
+            Console.WriteLine("-------------------------------------------------");
+            Console.WriteLine("Коефіцієнт невірних переходів Кнп \n"); //коефіцієнт невірних переходів
+            int[] d1 = { 0, 1, 2, 3, 3 };
+            double[] Cnd = new double[d1.Length]; //вибірка C з n по d 
+            double[] knp = new double[d1.Length];
+            for (int i = 0; i < d1.Length; i++)
+            {
+                Cnd[i] = Fact(n) / (Fact(d1[i]) * Fact(n - d1[i]));
+                knp[i] += (NPi1[i] / Cnd[i]);
+                knp[i] *= 0.25;
+                Console.WriteLine(knp[i]);
+            }
+
+            Console.WriteLine("-------------------------------------------------");
+            Console.WriteLine("У відсотках: \n");
+            double[] per = new double[knp.Length];
+            for (int i = 0; i < d1.Length; i++)
+            {
+                per[i] = (1 - knp[i]) * 100; //перевдення у %
+                Console.WriteLine(per[i] + "%");
+            }
+
+            Console.ReadLine();
+        }
+
+                                                                                                                                                                                            static long Fact(int n) //функція обчислення факторіалу
+                                                                                                                                                                                            {
+                                                                                                                                                                                                long x = 1;
+                                                                                                                                                                                                for (int i = 1; i <= n; i++)
+                                                                                                                                                                                                    x *= i;
+                                                                                                                                                                                                return x;
+                                                                                                                                                                                            }
+
+    }
+}
