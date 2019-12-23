1a4de573ff3c78de365b88b3094f33fd475f6853
Andrii Buzhak <andriy.buzhak.1@gmail.com>
2019-12-23 10:08:14 +0200

    Clean up of Lab_1,

--- MethodsAndToolsOfClusterAnalysis/Labs/Lab_1/Script.R
+++ MethodsAndToolsOfClusterAnalysis/Labs/Lab_1/Script.R
@@ -1,21 +1,14 @@
-# 1) Написати функцію, яка має три обов’язкові вхідні параметри: 
-# два вектори однакової довжини та назву методу, за допомогою якого  буде знайдена 
-# відстань між двома векторами. Функція повинна реалізувати п’ять відстаней.
-
-GetDistance = function(x, y, methodName = 'Euclidian', p = 2, W = NA)
+GetDistance = function(x, y, methodName = 'Euclidian', p = NA, W = NA)
 {
     return(switch(methodName,
-        'Euclidian' = sqrt(sum((x - y) * (x - y))),
+        'Euclidian' = sqrt(sum((x - y) ^ 2)),
         'Minkowski' = (sum(Mod(x - y) ^ p)) ^ (1 / p),
         'Manhattan' = sum(Mod(x - y)),
         'Chebyshev' = max(Mod(x - y)),
-        'Mahalonobis' = t(a) %*% W %*% (x - y)));
+        'Mahalonobis' = t(x - y) %*% W %*% (x - y)));
 }
 
-# 2) Використовуючи функції з п.1, написати функцію для обчислення 
-# матриці відстаней N об’єктів.
-
-CalculateDistanceMatrix = function(X, methodName = 'Euclidian', p = NA, W = NA)
+GetDistanceMatrix = function(X, methodName = 'Euclidian', p = NA, W = NA)
 {
     N = dim(X)[1];
     result = matrix(NA, nrow = N, ncol = N);
@@ -29,12 +22,9 @@ CalculateDistanceMatrix = function(X, methodName = 'Euclidian', p = NA, W = NA)
     return(result);
 }
 
-
-#2.2 Побудувати матрицю схожості
-
-CalculateSimilarityMatrix = function(X, methodName = 'Euclidian', p = NA, W = NA)
+GetSimilarityMatrix = function(X, methodName = 'Euclidian', p = NA, W = NA)
 {
-    distanceMatrix = CalculateDistanceMatrix(X);
+    distanceMatrix = GetDistanceMatrix(X);
 
     N = dim(X)[1];
 
@@ -49,35 +39,24 @@ CalculateSimilarityMatrix = function(X, methodName = 'Euclidian', p = NA, W = NA
     return(result);
 }
 
-CalculateSimilarityMatrix(c(2, 3, 7));
+testMatrix_1 = matrix(c(0, 0, 0, 1, 1, 2, 7, 5, 2), nrow = 3, ncol = 3, byrow = TRUE);
+print('==================================================================');
+print('Test matrix 1:');
+print(testMatrix_1);
+print('==================================================================');
+print('Distance matrix:');
+print(GetDistanceMatrix(testMatrix_1));
+print('==================================================================');
+print('Similarity matrix:');
+print(GetSimilarityMatrix(testMatrix_1));
+print('==================================================================');
 
 
-#3.	Згенерувати масив з 10 двовимірних точок в R^2,зобразити даний масив
-#   на графіку та побудувати матрицю відстаней для даного двовимірного масиву. 
-
-# двовимірна матриця згенерована з допомогою показникового розподілу
-Mat = matrix(rexp(20, rate = 0.6), 10, 2);
-plot(Mat);
-
-Mat;
-
-Rs = function(Mat, c = "1", p = NA, W = NA)
-{
-    N = dim(Mat)[1];
-    result = matrix(NA, N, N);
-
-    for (i in 1:N)
-    {
-        for (j in 1:N)
-        {
-            if (c == "1") { result[i, j] = GetDistance(Mat[i,], Mat[j,]); }
-            if (c == "2") { result[i, j] = GetDistance(Mat[i,], Mat[j,], 'Minkowski', p); }
-            if (c == "3") { result[i, j] = GetDistance(Mat[i,], Mat[j,], 'Manhattan'); }
-            if (c == "4") { result[i, j] = GetDistance(Mat[i,], Mat[j,], 'Chebyshev'); }
-            if (c == "5") { result[i, j] = GetDistance(Mat[i,], Mat[j,], 'Mahalonobis', NA, W); }
-        }
-    }
-
-    return(result);
-}
-Rs(Mat);
+testMatrix_2 = matrix(rexp(20, rate = 0.6), 10, 2);
+testMatrix_2 = testMatrix_2[order(testMatrix_2[,2]),];
+print('Test matrix 2:');
+print(testMatrix_2);
+plot(testMatrix_2);
+print('==================================================================');
+print('Distance matrix:');
+print(GetDistanceMatrix(testMatrix_2));
