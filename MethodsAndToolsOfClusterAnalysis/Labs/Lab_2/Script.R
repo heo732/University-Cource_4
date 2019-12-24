7274e2a39e2296891bcc7221acab8d914259a463
Andrii Buzhak <andriy.buzhak.1@gmail.com>
2019-12-24 15:04:35 +0200

    Clean up.

--- MethodsAndToolsOfClusterAnalysis/Labs/Lab_2/Script.R
+++ MethodsAndToolsOfClusterAnalysis/Labs/Lab_2/Script.R
@@ -1,3 +1,5 @@
+# 1
+
 mu1 = c(1, 1);
 mu2 = c(1, -9);
 mu3 = c(-7, -2);
@@ -26,82 +28,73 @@ legend("right", inset = c(-0.2, 0), legend = c("1", "2", "3"),
 
 #=====================================================================================
 
-# 2. Використовуючи метод k – середніх з трьома випадковими початковими центрами, 
-#побудувати кластери для всіх вибірок з 3N точками для k=2,3,4
-#Зобразити на графіках результати відповідної кластеризації
+# 2
 
-V = data.frame(c(a1[, 1], a2[, 1], a3[, 1]),
-               c(a1[, 2], a2[, 2], a3[, 2])) #об'єднання а1,а2,а3
+V = data.frame(c(x1[, 1], x2[, 1], x3[, 1]),
+               c(x1[, 2], x2[, 2], x3[, 2]));
 
-A1 = kmeans(V, centers = 3, iter.max = 10, nstart = 1) #метод к-середніх з 3 центрами
-A2 = kmeans(V, centers = 2, iter.max = 10, nstart = 1) #метод к-середніх з 2 центрами
-A3 = kmeans(V, centers = 4, iter.max = 10, nstart = 1) #метод к-середніх з 4 центрами
+A1 = kmeans(V, centers = 2, iter.max = 10, nstart = 1);
+A2 = kmeans(V, centers = 3, iter.max = 10, nstart = 1);
+A3 = kmeans(V, centers = 4, iter.max = 10, nstart = 1);
 
-plot(V, type = "p", col = A1$cluster)
-lines(A1$centers, type = "p", col = "blue")
+plot(V, type = "p", col = A1$cluster);
+lines(A1$centers, type = "p", col = "blue");
 
-plot(V, type = "p", col = A2$cluster)
-lines(A2$centers, type = "p", col = "blue")
+plot(V, type = "p", col = A2$cluster);
+lines(A2$centers, type = "p", col = "blue");
 
-plot(V, type = "p", col = A3$cluster)
-lines(A3$centers, type = "p", col = "blue")
+plot(V, type = "p", col = A3$cluster);
+lines(A3$centers, type = "p", col = "blue");
 
+#=====================================================================================
 
-#3. Використовуючи ієрархічну кластеризацію з трьома випадковими початковими центрами, 
-#побудувати кластери для всіх вибірок з 3N точками для k=2,3,4 кластерів. 
-#Зобразити на графіках результати відповідної кластеризації.
+# 3
 
-d = dist(V, method = "euclidean") #знайти матрицю відстаней
+d = dist(V, method = "euclidean");
 
-H1 = hclust(d) #ієрархічна кластеризація 
+H1 = hclust(d);
 
-#групуємо на 3 кластери
-sub_grp <- cutree(H1, k = 3)
+sub_grp = cutree(H1, k = 3);
 
-#вивід:
-table(sub_grp)
-fviz_cluster(list(data = V, cluster = sub_grp), geom = "point")
+table(sub_grp);
+factoextra::fviz_cluster(list(data = V, cluster = sub_grp), geom = "point");
 
-#групуємо на 2 кластери
-sub_grp1 <- cutree(H1, k = 2)
+sub_grp1 = cutree(H1, k = 2);
 
-table(sub_grp1)
-fviz_cluster(list(data = V, cluster = sub_grp1), geom = "point")
+table(sub_grp1);
+factoextra::fviz_cluster(list(data = V, cluster = sub_grp1), geom = "point");
 
-#групуємо на 4 кластери
-sub_grp2 <- cutree(H1, k = 4)
+sub_grp2 = cutree(H1, k = 4);
 
-table(sub_grp2)
-fviz_cluster(list(data = V, cluster = sub_grp2), geom = "point")
+table(sub_grp2);
+factoextra::fviz_cluster(list(data = V, cluster = sub_grp2), geom = "point");
 
+#=====================================================================================
 
+# 4
 
-#4. Використовуючи невеликий набір даних (N = 5), 
-#побудувати дендограму ієрархічної кластеризації.  
+V1 = V[1:5,];
 
-V1 = V[1:5,] #записуємо перші 5 елементів з V
+d1 = dist(V1);
+H2 = hclust(d1);
+sub_grp3 = cutree(H2, k = 2);
+table(sub_grp3);
 
-# повторюємо 3 завдання для побудови ієрархічної кластеризації над V1
-d1 = dist(V1)
-H2 = hclust(d1)
-sub_grp3 <- cutree(H2, k = 2)
-table(sub_grp3)
+hcd = ape::as.phylo(H2);
+plot(hcd, type = "fan");
 
-hcd <- as.phylo(H2) #для перетворення у вигляд дендограми
-plot(hcd, type = "fan") #type="radial" or "fan"
+#=====================================================================================
 
-#5. Для випадку K=3 у методі k – середніх та ієрархічної кластеризації 
-#для повного набору даних побудувати матрицю невідповідності. 
-#Використовуючи матрицю невідповідності CM, обчислити 
-#відсоток помилок в методах k – середніх та ієрархічної кластеризації
+# 5
 
-t <- table(A1$cluster, sub_grp)
-t
-P = permn(1:3)
-P
-for (i in 6) {
+t = table(A1$cluster, sub_grp);
+t;
+P = combinat::permn(1:3);
+P;
+for (i in 6)
+{
 
-    t1 = t[, P[[i]]]
-    AC = sum(diag(t1)) / sum(t1) * 100
+    t1 = t[, P[[i]]];
+    AC = sum(diag(t1)) / sum(t1) * 100;
 }
-AC
+AC;
