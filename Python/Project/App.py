288087e26d667f7e87a42dc645f0772b8c658376
Andrii Buzhak <andriy.buzhak.1@gmail.com>
2020-04-07 11:57:49 +0300

    Changed app main script.

--- Python/Project/App.py
+++ Python/Project/App.py
@@ -1,11 +1,22 @@
-import wpf
-from System.Windows import Application, Window
+import clr
+clr.AddReference("PresentationFramework")
+clr.AddReference("PresentationCore")
+from System.IO import File
+from System.Windows.Markup import XamlReader
+from System.Windows import Application
 from ViewModels.MainWindowViewModel import MainWindowViewModel
 
-class MyWindow(Window):
-    def __init__(self):
-        wpf.LoadComponent(self, 'MainWindow.xaml')
-        self.DataContext = MainWindowViewModel()
+class XamlLoader(object):
+    def __init__(self, xamlPath):
+        stream = File.OpenRead(xamlPath)
+        self.Root = XamlReader.Load(stream)
+        
+    def __getattr__(self, item):
+        """Maps values to attributes.
+        Only called if there *isn't* an attribute with this name
+        """
+        return self.Root.FindName(item)
 
-if __name__ == '__main__':
-    Application().Run(MyWindow())
+xaml = XamlLoader('MainWindow.xaml')
+xaml.Root.DataContext = MainWindowViewModel()
+Application().Run(xaml.Root)
