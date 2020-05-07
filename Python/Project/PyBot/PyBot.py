fabde0453baba53979b59fdf3b7b32f81749f159
Andrii Buzhak <andriy.buzhak.1@gmail.com>
2020-05-08 00:13:09 +0300

    Works changing of converter.

--- Python/Project/PyBot/PyBot.py
+++ Python/Project/PyBot/PyBot.py
@@ -2,6 +2,7 @@ import os, sys
 sys.path.append(os.path.join(os.path.dirname(__file__), "..\\..\\Project"))
 
 import telebot
+from telebot.types import InlineKeyboardMarkup, InlineKeyboardButton
 from ConvertersLogic.Enums.Temperature import Temperature
 from ConvertersLogic.Converters.TemperatureConverter import TemperatureConverter
 from ConvertersLogic.Enums.Mass import Mass
@@ -15,10 +16,30 @@ currentConverter = TemperatureConverter()
 currentInputUnit = Temperature.Celsius
 currentOutputUnit = Temperature.Kelvin
 
+#-----------------------------------------
+callback_converterTemperature = "cb_c_t"
+callback_converterMass = "cb_c_m"
+callback_converterCurrency = "cb_c_c"
+#-----------------------------------------
+callback_inputUnitCelsius = "cb_iu_c"
+callback_outputUnitCelsius = "cb_ou_c"
+callback_inputUnitFahrenheit = "cb_iu_f"
+callback_outputUnitFahrenheit = "cb_ou_f"
+callback_inputUnitKelvin = "cb_iu_k"
+callback_outputUnitKelvin = "cb_ou_k"
+#-----------------------------------------
+#-----------------------------------------
+
+def getKeyboardMarkup_Converters():
+    markup = InlineKeyboardMarkup()
+    markup.row_width = 3
+    markup.add(InlineKeyboardButton("Temperature", callback_data=callback_converterTemperature), InlineKeyboardButton("Mass", callback_data=callback_converterMass), InlineKeyboardButton("Currency", callback_data=callback_converterCurrency))
+    return markup
+
 @bot.message_handler(commands=["selectConverterType"])
 def selectConverterType(message):
     try:
-        pass
+        bot.send_message(message.chat.id, "Select converter type", reply_markup=getKeyboardMarkup_Converters())
     except Exception as e:
         bot.send_message(message.chat.id, e)
 
@@ -39,9 +60,37 @@ def selectOutputUnit(message):
 @bot.message_handler(content_types=["text"])
 def sendAnswer(message):
     try:
+        answer = "Converter: "
+
         if type(currentConverter) is TemperatureConverter:
-            bot.send_message(message.chat.id, str(currentConverter.convert(float(message.text), currentInputUnit, currentOutputUnit)))
+            #bot.send_message(message.chat.id, str(currentConverter.convert(float(message.text), currentInputUnit, currentOutputUnit)))
+            answer += "Temperature\n"
+        elif type(currentConverter) is MassConverter:
+            #bot.send_message(message.chat.id, str(currentConverter.convert(float(message.text), currentInputUnit, currentOutputUnit)))
+            answer += "Mass\n"
+        elif type(currentConverter) is CurrencyRates:
+            #bot.send_message(message.chat.id, str(currentConverter.convert(float(message.text), currentInputUnit, currentOutputUnit)))
+            answer += "Currency\n"
+        else:
+            bot.send_message(message.chat.id, "Something wrong.")
+
+        bot.send_message(message.chat.id, answer)
     except Exception as e:
         bot.send_message(message.chat.id, e)
+
+@bot.callback_query_handler(func=lambda call: True)
+def callback_query(call):
+    global currentConverter
+
+    if call.data == callback_converterTemperature:
+        currentConverter = TemperatureConverter()
+    elif call.data == callback_converterMass:
+        currentConverter = MassConverter()
+    elif call.data == callback_converterCurrency:
+        currentConverter = CurrencyRates()
+    else:
+        bot.send_message(call.chat.id, "Something wrong")
+        
+    bot.answer_callback_query(call.id, str(type(currentConverter)))
 
 bot.polling(none_stop=True)
