4fbed4faf8783ef915b2906088d0cccd5b557215
Andrii Buzhak <andriy.buzhak.1@gmail.com>
2020-05-07 12:29:58 +0300

    Added CurrencyConverter for PyBot.

--- Python/Project/PyBot/PyBot.py
+++ Python/Project/PyBot/PyBot.py
@@ -4,15 +4,17 @@ sys.path.append(os.path.join(os.path.dirname(__file__), "..\\..\\Project"))
 import telebot
 from ConvertersLogic.Enums.Temperature import Temperature
 from ConvertersLogic.Converters.TemperatureConverter import TemperatureConverter
+from forex_python.converter import CurrencyRates
 
 token = open("token.txt").read()
 bot = telebot.TeleBot(token)
+converter = CurrencyRates()
 
 @bot.message_handler(content_types=["text"])
 def lalala(message):
     try:
-        bot.send_message(message.chat.id, str(TemperatureConverter().convert(message.text, Temperature.Celsius, Temperature.Kelvin)))
-    except ValueError:
-        bot.send_message(message.chat.id, "Please, type a number.")
+        bot.send_message(message.chat.id, str(converter.convert("USD", "EUR", float(message.text))))
+    except Exception as e:
+        bot.send_message(message.chat.id, e)
 
 bot.polling(none_stop=True)
--- Python/Project/PyWpf/ViewModels/MainWindowViewModel.py
+++ Python/Project/PyWpf/ViewModels/MainWindowViewModel.py
@@ -1,5 +1,6 @@
 import os, sys, wpf
 sys.path.append(os.path.join(os.path.dirname(__file__), "..\\..\\..\\Project"))
+#sys.path.append("C:\\Python27\\Lib\\site-packages")
 
 from ViewModels.ViewModelBase import ViewModelBase
 from Command import Command
@@ -7,17 +8,16 @@ from ConvertersLogic.Enums.Temperature import Temperature
 from ConvertersLogic.Enums.Mass import Mass
 from ConvertersLogic.Converters.TemperatureConverter import TemperatureConverter
 from ConvertersLogic.Converters.MassConverter import MassConverter
+#from forex_python.converter import CurrencyRates
 from System.Windows import MessageBox
 
 class MainWindowViewModel(ViewModelBase):
     def __init__(self):
         ViewModelBase.__init__(self)
         self.WindowTitle = ""
-        self.ChangeWindowTitleCommand = Command(self.show)
-    
+        self.ChangeWindowTitleCommand = Command(self.setWindowTitle)
+        #self._currencyRates = CurrencyRates()
+
     def setWindowTitle(self):
         self.WindowTitle = "HELLO"
         self.RaisePropertyChanged("WindowTitle")
-
-    def show(self):
-        MessageBox.Show(str(MassConverter().convert(10051, Mass.Ounce, Mass.Gram)))
